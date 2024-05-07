c369c3472800a89e850dbdb52eb6237902c0b739
MCGom <100755492+MCGom@users.noreply.github.com>
2024-05-08 03:05:26 +0900

    ProductController

--- OtpishAI_Backend_Codes/src/main/java/otpishAI/otpishAI_Backend/controller/ProductPageController.java
+++ OtpishAI_Backend_Codes/src/main/java/otpishAI/otpishAI_Backend/controller/ProductPageController.java
@@ -1,10 +1,9 @@
 package otpishAI.otpishAI_Backend.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import otpishAI.otpishAI_Backend.entity.Product;
-import otpishAI.otpishAI_Backend.repository.ProductRepository;
 import otpishAI.otpishAI_Backend.service.ProductService;
-import otpishAI.otpishAI_Backend.service.RefreshTCheckService;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -31,7 +28,8 @@ public class ProductPageController {
     public ResponseEntity<?> listingProduct(@RequestParam(defaultValue = "") String genre,
                                                       @RequestParam(defaultValue = "0") String brandIds,
                                                       @RequestParam(defaultValue = "") String category,
-                                                      @PageableDefault(size = 20, sort = "id") Pageable pageable) {
+                                                      @PageableDefault(page = 0, size = 30, sort = "payment_N", direction = Sort.Direction.DESC) Pageable pageable) {
+        
         // 쉼표로 구분된 브랜드 ID 문자열을 정수 리스트로 변환
         List<Integer> brandList = Arrays.stream(brandIds.split(","))
                 .map(Integer::parseInt)
